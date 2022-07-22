d4816f5b9987c0e9885945f83322c78ce42e7c9e
AsiriZenith <asirisenith@gmail.com>
2022-07-22 23:54:29 +0530

    added signup form

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -3,15 +3,16 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatIconModule,
     MatInputModule,
+    MatCheckboxModule,
     MatFormFieldModule,
     MatDatepickerModule,
     MatNativeDateModule,
@@ -20,9 +21,10 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
     MatButtonModule,
     MatIconModule,
     MatInputModule,
+    MatCheckboxModule,
     MatFormFieldModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
   ],
 })
 export class MaterialModule {}
